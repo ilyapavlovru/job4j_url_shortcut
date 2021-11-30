d7be19b7a988f1dc787e223b0aa0ad1c8e8a1491
Ilya Pavlov <ilya.pavlov@list.ru>
2021-11-30 11:18:10 +0300

    Подключен Liquibase

--- src/main/java/ru/job4j/url_shortcut/UrlShortcutApplication.java
+++ src/main/java/ru/job4j/url_shortcut/UrlShortcutApplication.java
@@ -1,9 +1,12 @@
 package ru.job4j.url_shortcut;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class UrlShortcutApplication {
@@ -15,5 +18,13 @@ public class UrlShortcutApplication {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
     }
 }
